33fcabab33040cb644667e1acc6f5cd8a9f14fec
snugglepantz <burningice64@gmail.com>
2015-04-07 21:01:08 -0400

    notes

--- sassgulp/gulp/tasks/dev/sass.js
+++ sassgulp/gulp/tasks/dev/sass.js
@@ -3,10 +3,14 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var config = require('../../config');
 
-gulp.task('sassDev', ['clean'], function() {
+gulp.task('sassDev', ['clean'], function () {
   return gulp.src(config.sass.src)
       .pipe(sourcemaps.init())
-      .pipe(sass())
+      .pipe(sass({
+        includePaths: [
+          'bower/lib/susy/sass'
+        ]
+      }))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest(config.stage + config.css.dest));
 });
